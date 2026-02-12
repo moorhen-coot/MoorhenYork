1d4a321b209dd2dfa56a9646614e9b4f1c27eb91
Stuart McNicholas <stuart.mcnicholas@york.ac.uk>
2026-02-12 14:55:05 +0000

    Some changes for stricter typescript.

--- src/components/layouts/LigandLayout.tsx
+++ src/components/layouts/LigandLayout.tsx
@@ -62,7 +62,7 @@ export const LigandLayout: React.FC<LayoutProps> = (props) => {
             returnType: 'status',
             command: 'get_monomer_and_position_at',
             commandArgs: [ligandName, anyMolNo,
-                ...originState.map(coord => -coord)
+                ...originState.map((coord: number[]) => -coord)
             ]
         }, true) as moorhen.WorkerResponse<number>
         
